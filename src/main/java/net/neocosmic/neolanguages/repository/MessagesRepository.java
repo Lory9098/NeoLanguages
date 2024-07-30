16a1de457c1caf537e79a15e43202eb626da10dc
tecca <83712762+Lory9098@users.noreply.github.com>
2024-07-30 13:11:24 +0200

    Added getMessage uuid based

--- src/main/java/net/neocosmic/neolanguages/repository/MessagesRepository.java
+++ src/main/java/net/neocosmic/neolanguages/repository/MessagesRepository.java
@@ -2,8 +2,10 @@ package net.neocosmic.neolanguages.repository;
 
 import net.kyori.adventure.text.Component;
 import net.neocosmic.neolanguages.language.PlayerLanguage;
+import net.neocosmic.neolanguages.utils.PlayerCache;
 
 import java.util.HashMap;
+import java.util.UUID;
 
 //                                       // The message key enum
 public abstract class MessagesRepository<T extends Enum<?>> {
@@ -18,8 +20,16 @@ public abstract class MessagesRepository<T extends Enum<?>> {
         return messages.get(key).get(language);
     }
 
+    public String getMessage(UUID uuid, T key) {
+        return getMessage(PlayerCache.getPlayerLanguage(uuid), key);
+    }
+
     public Component getComponent(PlayerLanguage language, T key) {
         return Component.text(getMessage(language, key));
+    }
+
+    public Component getComponent(UUID uuid, T key) {
+        return getComponent(PlayerCache.getPlayerLanguage(uuid), key);
     }
 
 }
